23b9605bc9b1ac8304d56d952df264bde512f142
Harry Bray <harry.bray23@gmail.com>
2022-07-11 11:17:04 -0400

    teaching is now sorted by most recent date at the institution in rev chron order

--- write_cv_teaching.py
+++ write_cv_teaching.py
@@ -14,6 +14,8 @@ def compile_teaching(teaching_file, table_spacing,lwidth,rwidth):
     institutions_index_count_dict={}
     institution_txt_dict={}
 
+    ordering_institutions_dict={} 
+
     # build the set of distint institutions and keep track of how many
     # times taught at each institution (so that your code knows when to
     # close the table environment)
@@ -38,7 +40,11 @@ def compile_teaching(teaching_file, table_spacing,lwidth,rwidth):
     # (string)
 
     for institution in institutions_set:
+        ## start the table for each institution
         institution_txt_dict[institution]= wc.subheader_setup(institution, table_spacing, lwidth, rwidth)
+        
+        ## prepare the ordering_institutions_dict for later comparison
+        ordering_institutions_dict[institution]=0
 
     # add rows to the appropriate table in the .tex file for each entry of
     # the csv. There is a table for each institution
@@ -53,10 +59,10 @@ def compile_teaching(teaching_file, table_spacing,lwidth,rwidth):
             course='{\\normalfont %s:} %s' % (course_no, course)
 
         institution=row['Institution']
-        start_sem=row['Start Semester']
-        start_year=row['Start Year']
-        end_sem=row['End Semester']
-        end_year=row['End Year']
+        start_sem=row['Start Semester'][:2]
+        start_year=row['Start Year'][2:]
+        end_sem=row['End Semester'][:2]
+        end_year=row['End Year'][2:]
         note=row['Note']
         description=row['Description']
     
@@ -64,20 +70,42 @@ def compile_teaching(teaching_file, table_spacing,lwidth,rwidth):
 
         date=wc.format_date('', '', start_year, start_sem, '', '', end_year, end_sem, active=False)
 
+        ## trying to identify the most recent date
+
+        if int(start_year)>int(ordering_institutions_dict[institution]):
+            ordering_institutions_dict[institution]=start_year
+
         for given_institution in institutions_set:
             if institution==given_institution:
                 institutions_index_count_dict[given_institution]+=1
 
+                ## so you know when to close the table; boolean value that
+                ## comparees length of dicts
                 is_end=institutions_count_dict[given_institution]==institutions_index_count_dict[given_institution]
 
+                ## add text to the appropriate institution
+
                 institution_txt_dict[given_institution]+=wc.create_table_entry(course,date,is_end,role, '', note, description,False)
+
+
+
 
 
     # add everybody to the main string
 
 
-    for key in institutions_set:
-        teaching_txt+=institution_txt_dict[key]
+#    for key in institutions_set:
+#        teaching_txt+=institution_txt_dict[key]
+
+
+    #sort in reverse chronological order
+    sorted_dict=dict(sorted(ordering_institutions_dict.items(),key= lambda x:x[1],reverse=True))
+
+    #choose this option is you want to sort in chronological order
+#    sorted_dict=dict(sorted(ordering_institutions_dict.items(),key= lambda x:x[1]))
+
+    for entry in sorted_dict:
+        teaching_txt+=institution_txt_dict[entry]
 
     return teaching_txt
 
