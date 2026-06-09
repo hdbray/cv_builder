e101d7406a4529b3784f0bed3f4ec65de4e7e70d
Harry Bray <harry.bray23@gmail.com>
2026-06-09 13:30:27 -0400

    updates to talks, visits, mentoring and mentees

--- write_cv_referee.py
+++ write_cv_referee.py
@@ -7,22 +7,56 @@ def compile_referee(referee_file):
 
     referee_dict=wc.convert_csv_to_dict(referee_file)
 
+#    print(referee_dict)
+
+    track_referee_repeats={}
+
+    for entry in referee_dict:
+        journal_key=entry['Journal']
+        service_key=entry['Service (quick opinion, full report)']
+        if service_key=='full report':
+            if journal_key in track_referee_repeats:
+                journal_value=track_referee_repeats[journal_key]
+                journal_value+=1
+                track_referee_repeats.update({journal_key:journal_value})
+            else:
+                track_referee_repeats.update({journal_key:1})
+
+#    print(track_referee_repeats)
+
     referee_txt='''\myheader{Referee Reports} 
     \medskip
 
     '''
-    for i in range(len(referee_dict)-1):
-        referee_txt+=(referee_dict[i]['Journal'])
-        referee_txt+=', '
+    for journal_key in track_referee_repeats:
+        journal_value=track_referee_repeats[journal_key]
+        if journal_value>1: 
+            referee_txt+=journal_key+' (%s)'% (journal_value)
+            referee_txt+=', '
+        else: 
+            referee_txt+=journal_key+', '
+        
+    referee_txt=referee_txt[:-2]+'.'
 
-    referee_txt+=referee_dict[len(referee_dict)-1]['Journal']
-    referee_txt+='.'
+#    print(referee_txt)
+
+
+
+#    for i in range(len(referee_dict)-1):
+#        referee_txt+=(referee_dict[i]['Journal'])
+#        referee_txt+=', '
+#
+#    referee_txt+=referee_dict[len(referee_dict)-1]['Journal']
+#    referee_txt+='.'
+
 
 
 
 
 
     return referee_txt
+
+#compile_referee('csv_files/referee.csv')
 
 #
 #
